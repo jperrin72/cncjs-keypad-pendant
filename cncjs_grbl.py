4dead876d2a363182d855a1588730f5c5311cf99
Jerome Përrin <jperrin72@free.fr>
2019-06-24 12:11:40 +0200

    Halt command

--- cncjs_grbl.py
+++ cncjs_grbl.py
@@ -45,13 +45,14 @@ class CNCjsGrbl:
 											'Authorization': 'Bearer {}'.format(self.access_token.decode('utf-8')),
 											'content-type': 'application/json'
 											},data=auth_token)
-		auth_token=self.request.json()['token']
-		self.request = self.session.get('http://'+self.server_ip+':'+str(self.server_port)+'/api/machines',params={'token':'{}'.format(auth_token)})
+		self.http_token=self.request.json()['token']
+		self.request = self.session.get('http://'+self.server_ip+':'+str(self.server_port)+'/api/machines',params={'token':'{}'.format(self.http_token)})
 		self.machine=self.request.json()
-		self.request = self.session.get('http://'+self.server_ip+':'+str(self.server_port)+'/api/commands',params={'token':'{}'.format(auth_token)})
+		self.request = self.session.get('http://'+self.server_ip+':'+str(self.server_port)+'/api/commands',params={'token':'{}'.format(self.http_token)})
 		self.commands=self.request.json()
-		self.request = self.session.get('http://'+self.server_ip+':'+str(self.server_port)+'/api/mdi',params={'token':'{}'.format(auth_token)})
+		self.request = self.session.get('http://'+self.server_ip+':'+str(self.server_port)+'/api/mdi',params={'token':'{}'.format(self.http_token)})
 		self.api=self.request.json()
+		self.run_cmd('Halt')
 
 	def connect(self):
 
@@ -147,10 +148,10 @@ class CNCjsGrbl:
 												id=None,
 												binary=None))
 
-	def run_cmd(self):
+	def run_cmd(self, title):
+		'lookup command and start through http api'
+
 		'''
-
-
 		POST /api/commands/run/ad5309c1-61c7-4d6e-a55a-bf8a9ebc8610 HTTP/1.1
 		Host: 192.168.0.19:8000
 		Connection: keep-alive
@@ -176,206 +177,22 @@ class CNCjsGrbl:
 		Connection: keep-alive
 
 		{"taskId":"58gMuwjOp"}
-		---
-
-		GET /api/machines?_=1561315083326 HTTP/1.1
-		Host: 192.168.0.19:8000
-		Connection: keep-alive
-		Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiIsIm5hbWUiOiIiLCJpYXQiOjE1NjEzMTUwODIsImV4cCI6MTU2MzkwNzA4Mn0.FvLH6gCGob0B4J_7nNikXMhl3uqHLXGEgGK9Z1cQl9M
-		X-Requested-With: XMLHttpRequest
-		Cache-Control: no-cache
-		User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36
-		Accept: */*
-		Referer: http://192.168.0.19:8000/
-		Accept-Encoding: gzip, deflate
-		Accept-Language: fr-FR,fr;q=0.9,en-GB;q=0.8,en;q=0.7
-		Cookie: lang=en; connect.sid=s%3AB1Pa_cKSSMmy1cvU-DECLB_NtIpTlifp.A7FyWete1gfjj6pQdfVG1kQj7mgUdZST1315851Ebgk; io=mWb5JFAuZXwdZXtjAAAD
-
-		HTTP/1.1 200 OK
-		Set-Cookie: lang=en; path=/; expires=Tue, 23 Jun 2020 18:38:03 GMT
-		Content-Type: application/json; charset=utf-8
-		Content-Length: 153
-		ETag: W/"99-PjWaYBSJ7NN2CKjuCS7VSlk59Ig"
-		Vary: Accept-Encoding
-		Date: Sun, 23 Jun 2019 18:38:03 GMT
-		Connection: keep-alive
-
-		{"records":[{"id":"0fc57b97-3628-48a2-843b-8e3e597ff5e0","name":"OpenBuilds OX","limits":{"xmin":0,"xmax":418,"ymin":0,"ymax":1029,"zmin":0,"zmax":47}}]}
-				
-
-		------
-
-		GET /api/commands?_=1561315082859&paging=false HTTP/1.1
-		Host: 192.168.0.19:8000
-		Connection: keep-alive
-		Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiIsIm5hbWUiOiIiLCJpYXQiOjE1NjEzMTUwODIsImV4cCI6MTU2MzkwNzA4Mn0.FvLH6gCGob0B4J_7nNikXMhl3uqHLXGEgGK9Z1cQl9M
-		X-Requested-With: XMLHttpRequest
-		Cache-Control: no-cache
-		User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36
-		Accept: */*
-		Referer: http://192.168.0.19:8000/
-		Accept-Encoding: gzip, deflate
-		Accept-Language: fr-FR,fr;q=0.9,en-GB;q=0.8,en;q=0.7
-		Cookie: lang=en; connect.sid=s%3AB1Pa_cKSSMmy1cvU-DECLB_NtIpTlifp.A7FyWete1gfjj6pQdfVG1kQj7mgUdZST1315851Ebgk; io=mWb5JFAuZXwdZXtjAAAD
-
-		HTTP/1.1 200 OK
-		Set-Cookie: lang=en; path=/; expires=Tue, 23 Jun 2020 18:38:02 GMT
-		Content-Type: application/json; charset=utf-8
-		Content-Length: 325
-		ETag: W/"145-8Cm4XOzzzRcrL85mZjSnQ2scnds"
-		Vary: Accept-Encoding
-		Date: Sun, 23 Jun 2019 18:38:02 GMT
-		Connection: keep-alive
-
-		{"pagination":{"page":1,"pageLength":10,"totalRecords":2},"records":[{"id":"ad5309c1-61c7-4d6e-a55a-bf8a9ebc8610","mtime":1513531359625,"enabled":true,"title":"Halt","commands":"/usr/bin/sudo /sbin/halt"},{"id":"cd21c946-2799-4151-9ead-b1726814a79a","mtime":1560974944292,"enabled":true,"title":"Test","commands":"ls -l\n"}]}
-
-
-		------
-
-		GET /api/mdi?_=1561315083328 HTTP/1.1
-		Host: 192.168.0.19:8000
-		Connection: keep-alive
-		Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiIsIm5hbWUiOiIiLCJpYXQiOjE1NjEzMTUwODIsImV4cCI6MTU2MzkwNzA4Mn0.FvLH6gCGob0B4J_7nNikXMhl3uqHLXGEgGK9Z1cQl9M
-		X-Requested-With: XMLHttpRequest
-		Cache-Control: no-cache
-		User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36
-		Accept: */*
-		Referer: http://192.168.0.19:8000/
-		Accept-Encoding: gzip, deflate
-		Accept-Language: fr-FR,fr;q=0.9,en-GB;q=0.8,en;q=0.7
-		Cookie: lang=en; connect.sid=s%3AB1Pa_cKSSMmy1cvU-DECLB_NtIpTlifp.A7FyWete1gfjj6pQdfVG1kQj7mgUdZST1315851Ebgk; io=mWb5JFAuZXwdZXtjAAAD
-
-		HTTP/1.1 200 OK
-		Set-Cookie: lang=en; path=/; expires=Tue, 23 Jun 2020 18:38:03 GMT
-		Content-Type: application/json; charset=utf-8
-		ETag: W/"f38-OdHuUveqDQv/HaqcWlOOnRDs4KA"
-		Vary: Accept-Encoding
-		Content-Encoding: gzip
-		Date: Sun, 23 Jun 2019 18:38:03 GMT
-		Connection: keep-alive
-		Transfer-Encoding: chunked
-
-		a
-		..........
-		4f5
-		.Wmo.6..+..~+.."%.A>x...K..q....P$...%O.....}G..#;....6`.b..r.=........rS.._~m%....B.D.gX	.`N-.........%.c.z.J....a.M..U.....f3..S..g..h.........*Mm.~[..EI..l^>^.o.a.Uz...s..
-		.cc.$....<Q.....s.m...N...{.;.....|68..{}x|>ts`.....zHf..2IQ.T[Tfh.g.........Q...^_.....0.....n...}e.];.y....=..... ..O.....0.qS.'`n.......i.9~Zs...s.P.....e1..<.i......gER&..|{A.....F..Q:.F..'....Yf.......s..<U..{.............?..W...tk...'V,.......Z..^.....'..b..f.f:@g.?|.. *.V.p..."..J..v..\.=I...J.*..Z.h........M....nk.....\4.\L..a.-..^}.\..e...].-..|.... ...,=
-		lQf.F./!..tY%...T..*90\...K.a...N.r..]..m=@.{...[v.+#?."..I5.......,..d.j....P.....x....h......7...V......W...0O,H.>)o!S+.U2CkJz."W.SU&w.Ax..Cc|.T..}.l.iR..v..*.1Aq.WQ)]....t.&I!..x,J;..(A'..3R..)..b....m....9.Aq.....l.o.9._1.p.P . ...K..6...a4...Pa...CB=,x..F....|hA...F...).[.....I\VE...L..L.TWT.ly'bFB4....o.-WU..d.;|NT......bN...@.XRi.....m...7B.B..	.[:.>..Q.sO0...ap.i..X..v.!.......d.F....".b..S....F...c...3....8.....a|M./.M. ...8.O....s..&*....`E..Q..B...{....zN....a$	.....u....Q.....E..D.....B...Y,..Ri.a.r..C......{8
-		@2J.k,.~.}.L....c....G.<....5*.....{.
-		.
-		...T.P..+.r..#`..J*......;Q.nz...A........y.Gmz...!wEm..T.....p...sJ/......+..hi.....A..~.......,.@..,..-G........ji....p:(;..(.8.P.2.......0..lG..o.....D.8...
-		0
-
-		------
-
-
-		GET /api/commands?_=1561315082859&paging=false HTTP/1.1
-		Host: 192.168.0.19:8000
-		Connection: keep-alive
-		Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiIsIm5hbWUiOiIiLCJpYXQiOjE1NjEzMTUwODIsImV4cCI6MTU2MzkwNzA4Mn0.FvLH6gCGob0B4J_7nNikXMhl3uqHLXGEgGK9Z1cQl9M
-		X-Requested-With: XMLHttpRequest
-		Cache-Control: no-cache
-		User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36
-		Accept: */*
-		Referer: http://192.168.0.19:8000/
-		Accept-Encoding: gzip, deflate
-		Accept-Language: fr-FR,fr;q=0.9,en-GB;q=0.8,en;q=0.7
-		Cookie: lang=en; connect.sid=s%3AB1Pa_cKSSMmy1cvU-DECLB_NtIpTlifp.A7FyWete1gfjj6pQdfVG1kQj7mgUdZST1315851Ebgk; io=mWb5JFAuZXwdZXtjAAAD
-
-		HTTP/1.1 200 OK
-		Set-Cookie: lang=en; path=/; expires=Tue, 23 Jun 2020 18:38:02 GMT
-		Content-Type: application/json; charset=utf-8
-		Content-Length: 325
-		ETag: W/"145-8Cm4XOzzzRcrL85mZjSnQ2scnds"
-		Vary: Accept-Encoding
-		Date: Sun, 23 Jun 2019 18:38:02 GMT
-		Connection: keep-alive
-
-		{"pagination":{"page":1,"pageLength":10,"totalRecords":2},"records":[{"id":"ad5309c1-61c7-4d6e-a55a-bf8a9ebc8610","mtime":1513531359625,"enabled":true,"title":"Halt","commands":"/usr/bin/sudo /sbin/halt"},{"id":"cd21c946-2799-4151-9ead-b1726814a79a","mtime":1560974944292,"enabled":true,"title":"Test","commands":"ls -l\n"}]}GET /api/mdi?_=1561315083328 HTTP/1.1
-		Host: 192.168.0.19:8000
-		Connection: keep-alive
-		Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiIsIm5hbWUiOiIiLCJpYXQiOjE1NjEzMTUwODIsImV4cCI6MTU2MzkwNzA4Mn0.FvLH6gCGob0B4J_7nNikXMhl3uqHLXGEgGK9Z1cQl9M
-		X-Requested-With: XMLHttpRequest
-		Cache-Control: no-cache
-		User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36
-		Accept: */*
-		Referer: http://192.168.0.19:8000/
-		Accept-Encoding: gzip, deflate
-		Accept-Language: fr-FR,fr;q=0.9,en-GB;q=0.8,en;q=0.7
-		Cookie: lang=en; connect.sid=s%3AB1Pa_cKSSMmy1cvU-DECLB_NtIpTlifp.A7FyWete1gfjj6pQdfVG1kQj7mgUdZST1315851Ebgk; io=mWb5JFAuZXwdZXtjAAAD
-
-		HTTP/1.1 200 OK
-		Set-Cookie: lang=en; path=/; expires=Tue, 23 Jun 2020 18:38:03 GMT
-		Content-Type: application/json; charset=utf-8
-		ETag: W/"f38-OdHuUveqDQv/HaqcWlOOnRDs4KA"
-		Vary: Accept-Encoding
-		Content-Encoding: gzip
-		Date: Sun, 23 Jun 2019 18:38:03 GMT
-		Connection: keep-alive
-		Transfer-Encoding: chunked
-
-		a
-		..........
-		4f5
-		.Wmo.6..+..~+.."%.A>x...K..q....P$...%O.....}G..#;....6`.b..r.=........rS.._~m%....B.D.gX	.`N-.........%.c.z.J....a.M..U.....f3..S..g..h.........*Mm.~[..EI..l^>^.o.a.Uz...s..
-		.cc.$....<Q.....s.m...N...{.;.....|68..{}x|>ts`.....zHf..2IQ.T[Tfh.g.........Q...^_.....0.....n...}e.];.y....=..... ..O.....0.qS.'`n.......i.9~Zs...s.P.....e1..<.i......gER&..|{A.....F..Q:.F..'....Yf.......s..<U..{.............?..W...tk...'V,.......Z..^.....'..b..f.f:@g.?|.. *.V.p..."..J..v..\.=I...J.*..Z.h........M....nk.....\4.\L..a.-..^}.\..e...].-..|.... ...,=
-		lQf.F./!..tY%...T..*90\...K.a...N.r..]..m=@.{...[v.+#?."..I5.......,..d.j....P.....x....h......7...V......W...0O,H.>)o!S+.U2CkJz."W.SU&w.Ax..Cc|.T..}.l.iR..v..*.1Aq.WQ)]....t.&I!..x,J;..(A'..3R..)..b....m....9.Aq.....l.o.9._1.p.P . ...K..6...a4...Pa...CB=,x..F....|hA...F...).[.....I\VE...L..L.TWT.ly'bFB4....o.-WU..d.;|NT......bN...@.XRi.....m...7B.B..	.[:.>..Q.sO0...ap.i..X..v.!.......d.F....".b..S....F...c...3....8.....a|M./.M. ...8.O....s..&*....`E..Q..B...{....zN....a$	.....u....Q.....E..D.....B...Y,..Ri.a.r..C......{8
-		@2J.k,.~.}.L....c....G.<....5*.....{.
-		.
-		...T.P..+.r..#`..J*......;Q.nz...A........y.Gmz...!wEm..T.....p...sJ/......+..hi.....A..~.......,.@..,..-G........ji....p:(;..(.8.P.2.......0..lG..o.....D.8...
-		0
-
-		GET /api/version/latest?_=1561315083532 HTTP/1.1
-		Host: 192.168.0.19:8000
-		Connection: keep-alive
-		Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiIsIm5hbWUiOiIiLCJpYXQiOjE1NjEzMTUwODIsImV4cCI6MTU2MzkwNzA4Mn0.FvLH6gCGob0B4J_7nNikXMhl3uqHLXGEgGK9Z1cQl9M
-		X-Requested-With: XMLHttpRequest
-		Cache-Control: no-cache
-		User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36
-		Accept: */*
-		Referer: http://192.168.0.19:8000/
-		Accept-Encoding: gzip, deflate
-		Accept-Language: fr-FR,fr;q=0.9,en-GB;q=0.8,en;q=0.7
-		Cookie: lang=en; connect.sid=s%3AB1Pa_cKSSMmy1cvU-DECLB_NtIpTlifp.A7FyWete1gfjj6pQdfVG1kQj7mgUdZST1315851Ebgk; io=mWb5JFAuZXwdZXtjAAAD
-
-		HTTP/1.1 200 OK
-		Set-Cookie: lang=en; path=/; expires=Tue, 23 Jun 2020 18:38:03 GMT
-		Content-Type: application/json; charset=utf-8
-		Content-Length: 189
-		ETag: W/"bd-zAqdzZ+5V6JSOYyoe+33pMd/4Cs"
-		Vary: Accept-Encoding
-		Date: Sun, 23 Jun 2019 18:38:03 GMT
-		Connection: keep-alive
-
-		{"name":"cncjs","version":"1.9.20","description":"A web-based interface for CNC milling controller running Grbl, Marlin, Smoothieware, or TinyG","homepage":"https://github.com/cncjs/cncjs"}
-
 		'''
+	
+		if self.commands is not None:
+			for command in [rec for rec in self.commands['records'] if rec['title'] == title]:
+				pass
+			print('command:',str(command))
+			self.request = self.session.post('http://'+self.server_ip+':'+str(self.server_port)+'/api/commands/run/'+str(command['id']),headers={
+											'Authorization': 'Bearer {}'.format(self.access_token.decode('utf-8')),
+											'content-type': 'application/json'
+											},data=self.http_token)
+			return self.request.json()
+
+		return None
 
 	def wait(self):
 		while (self.active_state not in ['Idle','Alarm','PacketOK','Hold','Sleep']):
 			print("wait=",self.active_state)
 			self.sio.sleep(0.05)
-
-
-"""
-'server_ip': '127.0.0.1',
-'server_port': 8000,
-'serial_port': '/dev/tty.MALS',
-'serial_baud': 115200,
-'controller_type': 'Grbl',
-'user_id': '',
-'user_name': 'cncjs-pendant',
-'user_pass': '',
-'ws_secret': '$2a$10$6LHS.kcGOmdGcu2kmbTMUu',
-'sio': <socketio.client.Client object at 0x10ce2f2e8>,
-'access_token': b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IiIsIm5hbWUiOiJjbmNqcy1wZW5kYW50In0.aDWMfveVjKazAw1GvQ-XcxCE-vE4cfeYzTTdNIrdvck',
-'access_token_url_string': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IiIsIm5hbWUiOiJjbmNqcy1wZW5kYW50In0.aDWMfveVjKazAw1GvQ-XcxCE-vE4cfeYzTTdNIrdvck',
-'startup_data': {'loadedControllers': ['Grbl', 'Marlin', 'Smoothie', 'TinyG'], 'baudrates': [], 'ports': []},
-'serial_state': {'port': '/dev/tty.MALS', 'baudrate': 115200, 'controllerType': 'Grbl', 'inuse': True},
-'controller_settings': {'version': '', 'parameters': {}, 'settings': {}},
-'controller_state': {'status': {'activeState': '', 'mpos': {'x': '0.000', 'y': '0.000', 'z': '0.000'}, 'wpos': {'x': '0.000', 'y': '0.000', 'z': '0.000'}, 'ov': []}, 'parserstate': {'modal': {'motion': 'G0', 'wcs': 'G54', 'plane': 'G17', 'units': 'G21', 'distance': 'G90', 'feedrate': 'G94', 'program': 'M0', 'spindle': 'M5', 'coolant': 'M9'}, 'tool': '', 'feedrate': '', 'spindle': ''}},
-'feeder_status': 	{'hold': False, 'holdReason': None, 'queue': 0, 'pending': False, 'changed': False},
-'sender_status': 	{'sp': 1, 'hold': False, 'holdReason': None, 'name': '', 'context': {}, 'size': 0, 'total': 0, 'sent': 0, 'received': 0, 'startTime': 0, 'finishTime': 0, 'elapsedTime': 0, 'remainingTime': 0},
-'workflow_state': 'idle'}
-"""
 
